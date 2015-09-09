24dfd26233a2ef7805043b08cbc8be8fea240f74
svlcode <vio.slaniceanu@gmail.com>
2015-09-09 10:56:40 +0300

    Initial commint

---
+++ sources/PowerGenerator/PowerGenerator/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PowerGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseFile.Location = new System.Drawing.Point(565, 10);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFile.TabIndex = 0;
+            this.btnBrowseFile.Text = "Browse file";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(84, 12);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(475, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text file:";
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbResult.Location = new System.Drawing.Point(12, 89);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(628, 315);
+            this.rtbResult.TabIndex = 3;
+            this.rtbResult.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerate.Location = new System.Drawing.Point(565, 60);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Table name:";
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Location = new System.Drawing.Point(84, 40);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(240, 20);
+            this.txtTableName.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 416);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnBrowseFile);
+            this.Name = "MainForm";
+            this.Text = "Genereate Insert Scripts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTableName;
+    }
+}
+
---
+++ sources/PowerGenerator/PowerGenerator/MainForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PowerGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            var filePath = GetFilePath();
+            if(!string.IsNullOrWhiteSpace(filePath))
+            {
+                if(File.Exists(filePath))
+                {
+                    txtFile.Text = filePath;
+                    txtTableName.Text = GetTableNameFromFilePath(filePath);
+                }
+                else
+                {
+                    MessageBox.Show("Error - File does not exists!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error - Select a text file!");
+            }
+        }
+
+        private string GetTableNameFromFilePath(string filePath)
+        {
+            var fileName = new FileInfo(filePath).Name;
+            return "dbo." + fileName.Replace(".txt", "");
+        }
+
+        private string GetFilePath()
+        {
+            using (var dlg = new OpenFileDialog())
+            {
+                return dlg.ShowDialog() == DialogResult.OK ? dlg.FileName : null;
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(txtTableName.Text))
+            {
+                if (!string.IsNullOrWhiteSpace(txtFile.Text))
+                {
+                    GenerateInsertScripts(txtFile.Text);
+                }
+            }
+            
+        }
+
+        private void GenerateInsertScripts(string fileName)
+        {
+            var headers = ReadHeaders(fileName);
+            var dataRecords = ReadDataRecords(fileName);
+            var insertScripts = GenerateScripts(headers, dataRecords);
+            rtbResult.Text = insertScripts;
+        }
+
+        private string GenerateScripts(IEnumerable<string> headers, IEnumerable<IEnumerable<string>> dataRecords)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var dataRecord in dataRecords)
+            {
+                var inserScript = GetInsertScript(headers, dataRecord);
+                builder.Append(inserScript);
+                builder.Append(Environment.NewLine);
+                builder.Append(Environment.NewLine);
+            }
+
+            return builder.ToString();
+        }
+
+        private string GetInsertScript(IEnumerable<string> headers, IEnumerable<string> dataRecord)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(GenerateInsertFieldsPart(headers.ToList()));
+            builder.Append(GenerateInsertValuesPart(dataRecord.ToList()));
+            return builder.ToString();
+        }
+
+        private string GenerateInsertValuesPart(List<string> dataRecord)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("VALUES (");
+            builder.Append(Environment.NewLine);
+            for (int i = 0; i < dataRecord.Count; i++)
+            {
+                var value = dataRecord[i];
+                double number;
+                if (!double.TryParse(value, out number))
+                {
+                    value = "'" + value + "'";
+                }
+                builder.Append(value);
+                if (i < dataRecord.Count - 1)
+                {
+                    builder.Append(",");
+                }
+                builder.Append(Environment.NewLine);
+            }
+            builder.Append(")");
+            builder.Append(Environment.NewLine);
+            return builder.ToString();
+        }
+
+        private string GenerateInsertFieldsPart(List<string> headers)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(string.Format("INSERT INTO {0} (", txtTableName.Text));
+            for (int i = 0; i < headers.Count; i++)
+            {
+                builder.Append(headers[i]);
+                if(i < headers.Count -1)
+                {
+                    builder.Append(",");
+                }
+                builder.Append(Environment.NewLine);
+            }
+            builder.Append(")");
+            builder.Append(Environment.NewLine);
+            return builder.ToString();
+        }
+
+        private IEnumerable<IEnumerable<string>> ReadDataRecords(string fileName)
+        {
+            List<IEnumerable<string>> dataLines = new List<IEnumerable<string>>();
+            var rawLines = File.ReadAllLines(fileName);
+            for (int i = 1; i < rawLines.Length; i++)
+            {
+                var line = GetDataRecord(rawLines[i]);
+                dataLines.Add(line);
+            }
+            return dataLines;
+        }
+
+        private IEnumerable<string> GetDataRecord(string rawLine)
+        {
+            List<string> dataRecord = new List<string>();
+            var lines = Regex.Split(rawLine, ",");
+            var processedLines = GetProcessedLines(lines);
+            dataRecord.AddRange(processedLines);
+            return dataRecord;
+        }
+
+        private IEnumerable<string> GetProcessedLines(IEnumerable<string> rawLines)
+        {
+            List<string> lines = new List<string>();
+            foreach (var rawLine in rawLines)
+            {
+                var processedLine = GetProcessedLine(rawLine);
+                lines.Add(processedLine);
+            }
+            return lines;
+        }
+
+        private string GetProcessedLine(string rawLine)
+        {
+            var line = rawLine.Trim('\t');
+            line = line.Trim();
+            return line;
+        }
+
+        private IEnumerable<string> ReadHeaders(string fileName)
+        {
+            List<string> list = new List<string>();
+
+            var firstLine = File.ReadLines(fileName).FirstOrDefault();
+            if(firstLine != null)
+            {
+                var rawLines = Regex.Split(firstLine, ",");
+                var processedLines = GetProcessedLines(rawLines);
+                list.AddRange(processedLines);
+            }
+            return list;
+        }
+
+    }
+}
